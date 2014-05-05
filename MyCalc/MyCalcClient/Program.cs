7debc2c114375124ce837c5db09ebe5d9fc48d5b
saburah-samples <saburah2@gmail.com>
2014-05-05 20:46:32 +0400

    initial commit

---
+++ MyCalc/MyCalcClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyCalcClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyCalcSvcProxy.ISimpleCalculator proxy = new MyCalcSvcProxy.SimpleCalculatorClient();
+            Console.WriteLine("5+5=" + proxy.Add(5, 6).ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyCalc/MyCalcSvc/SimpleCalculator.cs
@@ -0,0 +1,24 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="SimpleCalculator.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace MyCalcSvc
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class SimpleCalculator: ISimpleCalculator
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
